b272a0b741093bbfa5071408e59b4588dc7429ef
Richard Chuong <93078660+chuongCode@users.noreply.github.com>
2025-08-15 20:46:10 +0000

    delete old featurepoint marker type

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,14 +1,3 @@
-export type FeaturePoint = {
-  id: string;
-  type: "Feature";
-  geometry: { type: "Point"; coordinates: [number, number] };
-  properties: { title?: string };
-};
-export type FeatureCollection = {
-  type: "FeatureCollection";
-  features: FeaturePoint[];
-};
-
 export type LiveUser = {
   id: string;
   name?: string;
